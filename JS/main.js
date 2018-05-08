bbcae691e3d835193c86d2c8b89f4f25065ba1cf
Adam Stasiun <a.stasiun@icloud.com>
2018-05-08 18:06:29 +0200

    added comments on posts

--- JS/main.js
+++ JS/main.js
@@ -3,18 +3,29 @@ $(document).ready(function () {
     setDate();
 });
 
-function fadeDiv() {
+function fadeDiv(e) {
     $("#show").click(function () {
         $(".searchbar").slideToggle("slow");
         $('.searchbar').removeClass('hidden');
+        e.preventDefault;
     });
 }
 
 function setDate() {
 
-var today = new Date();
-var year = today.getFullYear();
+    var today = new Date();
+    var year = today.getFullYear();
 
-var rok = document.getElementById('mainfooter');
-rok.innerHTML = '<p>Copyright &copy;' + year + ' Adam Stasiun </p>';
+    var rok = document.getElementById('mainfooter');
+    rok.innerHTML = '<p>Copyright &copy;' + year + ' Adam Stasiun </p>';
 }
+
+//$(function loadBlog(e) {
+//    $('#blogcontentloaded').load('/blog/page1.html');
+//    e.preventDefault();
+//    return false;
+//});
+
+$(window).on("load", function () {
+    $('#blogcontentloaded').load('/blog/page1.html');
+});
